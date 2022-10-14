66e92523568108ceb2c3dace40369e406268040d
ashish <asespandey2@gmail.com>
2022-10-14 07:45:29 -0700

    first

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+import random
+
 urlpatterns = [
     path("",views.indexPage,name="home"),
     path("login/",views.loginUser,name="login"),
     path("addtodo/",views.addtodoPage,name="addtodo"),
     path("logout/",views.logoutUser,name="logout"),
     path("signin/",views.signInUser,name="signin"),
+    path("delete/sjbcsxx   122443 dbnxcx x234242 cviuv esjcerfh ucbdcnj d8hi45928cvsdscsdw24334432424v 31y21 cv  928ej vddffdxccxbvdc wddxfgdxfgfdsdw9i  k3rwedc/<str:pk>",views.deleteTodo,name="delete"),
+    path("update/<str:pk>",views.updateTodo,name="update"),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -31,31 +31,34 @@ def loginUser(request):
     else:
         return redirect("home")
 
-
 def signInUser(request):
+    error_message=True
     if request.method=="POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
         email = request.POST.get('email')
-
-        user = User.objects.create_user(username, email, password)
-        user.save()
-        loggedInUser = authenticate(username=username, password=password)
-        if loggedInUser is not None:
-            login(request, loggedInUser)
-            return redirect("/")
-        else:
-            return render(request, 'login.html')
-        return redirect("login")
+        try:
+           user = User.objects.create_user(username, email, password)
+           user.save()
+           loggedInUser = authenticate(username=username, password=password)
+           if loggedInUser is not None:
+              login(request, loggedInUser)
+              return redirect("/")
+        except:
+           error_message=False
     if request.user.is_anonymous:
-        return render(request, 'signin.html')
+        return render(request, 'signin.html',)
     else:
         return redirect("home")
-  
+    
+    
 
 
 
 def addtodoPage(request):
+    if request.user.is_anonymous:
+            return redirect("login")
+  
     if request.method == "POST":
         title = request.POST.get('title')
         description = request.POST.get('description')
@@ -70,3 +73,36 @@ def addtodoPage(request):
 def logoutUser(request):
     logout(request)
     return redirect("/login")
+
+
+
+
+def deleteTodo(request,pk):
+    if request.user.is_anonymous:
+        return redirect("/login")
+    todo=todoModel.objects.get(id=pk)
+    if request.method == "POST":
+        todo.delete()
+        return redirect("home")
+    context={ "todo":todo }
+
+    return render(request, 'delete.html',context)
+
+
+
+
+def updateTodo(request,pk):
+    if request.user.is_anonymous:
+        return redirect("/login")
+    todoToUpdate=todoModel.objects.get(title=pk)
+    
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        todoToUpdate.title=title
+        todoToUpdate.description=description
+        todoToUpdate.save()
+        return redirect("home")
+    return render(request,"update.html",{"todoToUpdate":todoToUpdate})
+    
+   
